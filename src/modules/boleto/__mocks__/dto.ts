bc5edebfcca8abba9099a1817f7f87d1d47484bc
Eduardo Conti <eduardo.conti@formaturismo.tech>
2023-05-26 23:01:57 -0300

    test: adiciona testes controller boleto

--- src/modules/boleto/__mocks__/dto.ts
+++ src/modules/boleto/__mocks__/dto.ts
@@ -1,5 +1,6 @@
 import type { IGerarBoletoUseCaseInput } from '@boleto/domain/use-cases';
 import type { IGerarBoletoUseCaseOutput } from '@boleto/domain/use-cases';
+import type { GerarBoletoInput } from '@boleto/presentation/dto/gerar-boleto-input.dto';
 
 export const mockGerarBoletoUseCaseInput: IGerarBoletoUseCaseInput = {
   nomeDevedor: 'Eduardo Conti',
@@ -20,4 +21,13 @@ export const mockGerarBoletoUseCaseOutput: IGerarBoletoUseCaseOutput = {
   id: 'b85381d7-174f-4c0a-a2c8-aa93a399965d',
   linhaDigitavel: '34191.79001 01043.510047 91020.150008 5 88110000099999',
   status: 'PENDENTE',
+};
+
+export const mockGerarBoletoInput: GerarBoletoInput = {
+  nome_devedor: 'Eduardo Conti',
+  id_cobranca: '123',
+  email: 'es.eduardoconti@gmail.com',
+  id_psp: '123',
+  valor: 9000,
+  data_vencimento: new Date('2023-05-27T01:54:46.698Z'),
 };
--- src/modules/boleto/app/use-cases/index.ts
+++ src/modules/boleto/app/use-cases/index.ts
@@ -1,1 +1,2 @@
 export * from './gerar-boleto.use-case';
+export * from './salvar-csv-cobranca.use-case';
